3d1be9ea8daf00ab5c19c79c89fc3245683934f3
Fernando dos Santos Santana <nando.fds5@gmail.com>
2022-07-30 20:26:50 -0300

    Upload

--- Exercicios/#002-Retornando mensagem na tela do usuario.py
+++ Exercicios/#002-Retornando mensagem na tela do usuario.py
@@ -1,4 +1,17 @@
 # Retornando mensagem de boas vindas ao usuário
-str = input('Digite seu nome: ')
-retorno = 'É um prazer te conhecer, '
-print(retorno + str + "!")
+
+# Forma 1
+nome = input('Digite seu nome: ')
+retorno = 'É um prazer te conhecer,'
+print(retorno, nome + '!')
+
+# Forma 2
+nome = input('Digite seu nome: ')
+retorno = 'É um prazer te conhecer,'
+print(retorno, '{}!'.format(nome))
+
+# Forma 2 com mais de uma variavel
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+retorno = 'É um prazer te conhecer,'
+print(retorno, '{} {}!'.format(nome, sobrenome))
